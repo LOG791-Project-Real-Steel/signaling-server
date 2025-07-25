f983c9a6356cfbe89de9601ccb6f7af0bc0ddb67
Adam Mihajlovic <mihajlovicadam@gmail.com>
2025-07-24 20:43:17 -0400

    yes, hello, whatsup

--- SignalingServer/Program.cs
+++ SignalingServer/Program.cs
@@ -11,11 +11,13 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 List<WebSocket> webSockets = [];
+List<WebSocket> webSockets2 = [];
 
 app.UseWebSockets();
 app.MapStaticAssets();
 
 app.MapGet("/robot/client", () => Results.File("~/index.html", "text/html"));
+app.MapGet("/robot/client2", () => Results.File("~/index2.html", "text/html"));
 
 app.Map("/robot/signaling", async context =>
 {
@@ -63,6 +65,58 @@ app.Map("/robot/signaling", async context =>
     {
         socket.Dispose();
         webSockets.Remove(socket);
+    }
+
+    Console.WriteLine("WebSocket disconnected.");
+});
+
+
+app.Map("/robot/signaling2", async context =>
+{
+    if (!context.WebSockets.IsWebSocketRequest)
+    {
+        context.Response.StatusCode = 400;
+        return;
+    }
+
+    var socket = await context.WebSockets.AcceptWebSocketAsync();
+    var remoteIP = context.Connection.RemoteIpAddress?.ToString();
+
+    webSockets2.Add(socket);
+
+    Console.WriteLine($"WebSocket connected from {remoteIP}");
+
+    var buffer = new byte[8192];
+    try
+    {
+        while (socket.State == WebSocketState.Open)
+        {
+            var result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            if (result.MessageType == WebSocketMessageType.Close)
+            {
+                await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closed", CancellationToken.None);
+                break;
+            }
+
+            var msg = new ArraySegment<byte>(buffer, 0, result.Count);
+            foreach (WebSocket ws in webSockets2)
+            {
+                if (ws.State == WebSocketState.Open && ws != socket)
+                {
+                    await ws.SendAsync(msg, result.MessageType, result.EndOfMessage, CancellationToken.None);
+                }
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+        return;
+    }
+    finally
+    {
+        socket.Dispose();
+        webSockets2.Remove(socket);
     }
 
     Console.WriteLine("WebSocket disconnected.");
